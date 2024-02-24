20153a94b7e2cd9d725f6fac72ff6fd20062f2a4
Mike Will <myQwil@gmail.com>
2024-02-24 17:15:39 -0500

    Make build process more scalable

--- build.zig
+++ build.zig
@@ -1,22 +1,47 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-	const target = b.standardTargetOptions(.{});
-	const optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *std.Build) !void {
+	var buf: [64]u8 = undefined;
 
-	const hello = b.addSharedLibrary(.{
-		.name = "helloworld",
-		.root_source_file = .{ .path = "src/helloworld.zig" },
-		.target = target,
-		.optimize = optimize,
-	});
-	hello.addIncludePath(.{ .cwd_relative = "src" });
-	hello.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
+	std.debug.print("\n---------- Externals ----------\n", .{});
+	{
+		const target = b.standardTargetOptions(.{});
+		const optimize = b.standardOptimizeOption(.{});
 
-	const install = b.addInstallFile(hello.getOutputSource(), "helloworld.pd_linux");
-	const paste = b.addInstallFile(hello.getOutputSource(), "../pd/helloworld.pd_linux");
-	install.step.dependOn(&hello.step);
-	paste.step.dependOn(&install.step);
+		const sources = [_][]const u8 {"helloworld"};
+		for (sources) |source| {
+			var s = try std.fmt.bufPrint(&buf, "src/{s}.zig", .{source});
+			const artifact = b.addSharedLibrary(.{
+				.name = source,
+				.root_source_file = .{ .path = s },
+				.target = target,
+				.optimize = optimize,
+			});
+			artifact.addIncludePath(.{ .cwd_relative = "src" });
+			artifact.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
 
-	b.getInstallStep().dependOn(&paste.step);
+			s = try std.fmt.bufPrint(&buf, "{s}.pd_linux", .{source});
+			std.debug.print("{s}\n", .{s});
+
+			const install = b.addInstallFile(artifact.getOutputSource(), s);
+			install.step.dependOn(&artifact.step);
+			b.getInstallStep().dependOn(&install.step);
+		}
+	}
+
+	std.debug.print("\n---------- Help files ----------\n", .{});
+	{
+		const dir = try std.fs.cwd().openIterableDir("help", .{});
+		var iter = dir.iterate();
+		while (try iter.next()) |file| {
+			if (file.kind != .file) {
+				continue;
+			}
+			std.debug.print("{s}\n", .{file.name});
+			const s = try std.fmt.bufPrint(&buf, "help/{s}", .{file.name});
+			b.installFile(s, file.name);
+		}
+	}
+
+	std.debug.print("\n", .{});
 }
