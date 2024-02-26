f1e9c4c814cb9c2828fb63ef754306b7ac8bfc69
Mike Will <myQwil@gmail.com>
2024-02-26 08:48:31 -0500

    Put functions in the struct
    This seems to be the more idiomatic approach in Zig.

--- src/helloworld.zig
+++ src/helloworld.zig
@@ -3,38 +3,43 @@ const c = @cImport({
 });
 
 // -------------------------------- hello world --------------------------------
-var helloworld_class: ?*c.t_class = null;
-
 const t_helloworld = extern struct {
-	x_obj: c.t_object,
-	x_s: *c.t_symbol,
+	const Self = @This();
+	var class: ?*c.t_class = undefined;
+
+	base: c.t_object,
+	sym: *c.t_symbol,
+
+	fn bang(self: *Self) void {
+		c.post("Hello %s!", self.sym.s_name);
+	}
+
+	fn float(self: *Self, f: c.t_float) void {
+		c.outlet_float(self.base.te_outlet, f * 2);
+	}
+
+	fn symbol(self: *Self, s: *c.t_symbol) void {
+		self.sym = s;
+	}
+
+	fn new() *Self {
+		const self: *Self = @ptrCast(c.pd_new(class));
+		_ = c.outlet_new(&self.base, &c.s_float);
+		self.sym = c.gensym("world");
+		return self;
+	}
+
+	fn setup() void {
+		class = c.class_new(c.gensym("helloworld"),
+			@as(c.t_newmethod, @ptrCast(&Self.new)), null,
+			@sizeOf(Self), c.CLASS_DEFAULT, 0);
+
+		c.class_addbang(class, @as(c.t_method, @ptrCast(&Self.bang)));
+		c.class_addfloat(class, @as(c.t_method, @ptrCast(&Self.float)));
+		c.class_addsymbol(class, @as(c.t_method, @ptrCast(&Self.symbol)));
+	}
 };
 
-fn helloworld_bang(x: *t_helloworld) void {
-	c.post("Hello %s!", x.x_s.s_name);
-}
-
-fn helloworld_float(x: *t_helloworld, f: c.t_float) void {
-	c.outlet_float(x.x_obj.te_outlet, f * 2);
-}
-
-fn helloworld_symbol(x: *t_helloworld, s: *c.t_symbol) void {
-	x.x_s = s;
-}
-
-fn helloworld_new() ?*anyopaque {
-	const x: *t_helloworld = @ptrCast(c.pd_new(helloworld_class));
-	_ = c.outlet_new(&x.x_obj, &c.s_float);
-	x.x_s = c.gensym("world");
-	return x;
-}
-
 export fn helloworld_setup() void {
-	helloworld_class = c.class_new(c.gensym("helloworld"),
-		@as(c.t_newmethod, @ptrCast(&helloworld_new)), null,
-		@sizeOf(t_helloworld), c.CLASS_DEFAULT, 0);
-
-	c.class_addbang(helloworld_class, @as(c.t_method, @ptrCast(&helloworld_bang)));
-	c.class_addfloat(helloworld_class, @as(c.t_method, @ptrCast(&helloworld_float)));
-	c.class_addsymbol(helloworld_class, @as(c.t_method, @ptrCast(&helloworld_symbol)));
+	t_helloworld.setup();
 }
