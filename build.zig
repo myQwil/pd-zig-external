52bbf2faf7eb97f98eb04c35fb8916b5c0a3da35
Mike Will <myQwil@gmail.com>
2024-05-19 04:46:36 -0400

    build: replace bufPrint with b.fmt

--- build.zig
+++ build.zig
@@ -1,49 +1,40 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
-	var buf: [64]u8 = undefined;
+const externals = [_][]const u8 {
+	"helloworld"
+};
 
-	std.debug.print("\n---------- Externals ----------\n", .{});
-	{
-		const target = b.standardTargetOptions(.{});
-		const optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *std.Build) void {
+	const target = b.standardTargetOptions(.{});
+	const optimize = b.standardOptimizeOption(.{});
 
-		const sources = [_][]const u8 {"helloworld"};
-		for (sources) |source| {
-			var s = try std.fmt.bufPrint(&buf, "src/{s}.zig", .{source});
-			const artifact = b.addSharedLibrary(.{
-				.name = source,
-				.root_source_file = .{ .path = s },
-				.target = target,
-				.optimize = optimize,
-				.link_libc = true,
-				.pic = true
-			});
-			artifact.addIncludePath(.{ .cwd_relative = "src" });
-			artifact.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
+	for (externals) |e| {
+		const artifact = b.addSharedLibrary(.{
+			.name = e,
+			.root_source_file = .{ .path = b.fmt("src/{s}.zig", .{e}) },
+			.target = target,
+			.optimize = optimize,
+			.link_libc = true,
+			.pic = true,
+		});
+		artifact.addIncludePath(.{ .cwd_relative = "src" });
+		artifact.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
 
-			s = try std.fmt.bufPrint(&buf, "{s}.pd_linux", .{source});
-			std.debug.print("{s}\n", .{s});
-
-			const install = b.addInstallFile(artifact.getEmittedBin(), s);
-			install.step.dependOn(&artifact.step);
-			b.getInstallStep().dependOn(&install.step);
-		}
+		const install = b.addInstallFile(artifact.getEmittedBin(),
+			b.fmt("{s}.pd_linux", .{e}));
+		install.step.dependOn(&artifact.step);
+		b.getInstallStep().dependOn(&install.step);
 	}
+	installHelpFiles(b) catch {};
+}
 
-	std.debug.print("\n---------- Help files ----------\n", .{});
-	{
-		const dir = try std.fs.cwd().openDir("help", .{ .iterate = true });
-		var iter = dir.iterate();
-		while (try iter.next()) |file| {
-			if (file.kind != .file) {
-				continue;
-			}
-			std.debug.print("{s}\n", .{file.name});
-			const s = try std.fmt.bufPrint(&buf, "help/{s}", .{file.name});
-			b.installFile(s, file.name);
+fn installHelpFiles(b: *std.Build) !void {
+	const dir = try std.fs.cwd().openDir("help", .{ .iterate = true });
+	var iter = dir.iterate();
+	while (try iter.next()) |file| {
+		if (file.kind != .file) {
+			continue;
 		}
+		b.installFile(b.fmt("help/{s}", .{file.name}), file.name);
 	}
-
-	std.debug.print("\n", .{});
 }
